35252d1dfebe0a56b90c886b158602c62accfd91
HarryYC <sssbug@gmail.com>
2016-12-05 09:28:21 -0800

    first commit

---
+++ grant.py
@@ -0,0 +1,55 @@
+import subprocess
+import time
+import sys
+import pexpect
+
+def remote(ip, username, password, script):
+    print ("ssh " + username + "@" + ip)
+    ssh_newkey = 'Are you sure you want to continue connecting'
+    child = pexpect.spawn("ssh " + username + "@" + ip + " " + script)
+    i = child.expect([pexpect.TIMEOUT, ssh_newkey, 'password'])
+    if i == 0: # Timeout
+        print 'ERROR!'
+        print 'SSH could not login. Here is what SSH said:'
+        print child.before, child.after
+        return None
+    if i == 1: # SSH does not have the public key. Just accept it.
+        child.sendline ('yes')
+        child.expect ('password: ')
+        i = child.expect([pexpect.TIMEOUT, 'password: '])
+        if i == 0: # Timeout
+          print 'ERROR!'
+          print 'SSH could not login. Here is what SSH said:'
+          print child.before, child.after
+        return None
+    if i == 2:
+        child.sendline(password)
+        child.sendline("")
+        return child
+
+def generateScript(userList, loginPassword):
+    script = ""
+    with open(userList) as f:
+        for line in f.readlines():
+            words = line.split(" ")
+
+            username = words[0];
+            password = words[1].strip();
+
+            script += "echo " + str(loginPassword) + "| sudo -S useradd -g users -d /home/" \
+                    + username + " -s /bin/bash -p \"$(echo \"" + str(password) \
+                    + "\" | openssl passwd -1 -stdin)\" " + username + ";"
+    return script
+
+if __name__ == "__main__":
+    serverList = "/home/test/server.txt"
+    userList = "/home/test/user.txt"
+    with open(serverList) as f:
+        for line in f.readlines():
+            words = line.split(" ")
+
+            ip = words[0];
+            loginName = words[1];
+            loginPassword = words[2].strip();
+            script = generateScript(userList, loginPassword)
+            remote(ip, loginName, loginPassword, script)
---
+++ sshUser.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+if [ "$#" -ne 3 ] || ! [ -f "$1" ] || ! [ -f "$2" ] || [ $3 != "grant" -a $3 != "revoke" ]; then
+  echo "Usage: $0 SERVERLIST USERLIST grant/revoke"
+  echo "Example: $0 server.txt user.txt grant"
+  exit 1
+fi
+
+SERVERLIST="$1"
+USERLIST="$2"
+LOGPATH="/var/log/sshUser.log"
+echo -e "$(date)\n" >> ${LOGPATH}
+
+#loop each server in SERVERLIST
+while IFS=" " read -r IP LOGINNAME LOGINPW
+do
+  SCRIPT=""
+  echo -e "\n$IP" >> ${LOGPATH}
+  #loop each user in USERLIST
+  while IFS=" " read -r USERNAME USERPW
+  do
+    if [ "$3" = "grant" ]; then
+      #add user
+      SCRIPT="echo "${LOGINPW}" | sudo -S useradd \
+      -g users \
+      -d /home/${USERNAME} \
+      -s /bin/bash \
+      -p '$(echo "${USERPW}" | openssl passwd -1 -stdin)' \
+      ${USERNAME};"${SCRIPT}
+    else
+      #delete user
+      SCRIPT="echo "${LOGINPW}" | sudo -S userdel ${USERNAME};"${SCRIPT}
+    fi
+  done < "$USERLIST"
+  #execute the command on remote server and save the log file
+
+  sshpass -p "${LOGINPW}" ssh -o StrictHostKeyChecking=no -l ${LOGINNAME} ${IP} "${SCRIPT}" < /dev/null\
+  >>$LOGPATH 2>&1 || true
+done < "$SERVERLIST"
+
+echo -e "\n-----------DONE--------------" >> ${LOGPATH}
+
+
+
+#the StrictHostKeyChecking option  disable the host key check 
+#and automatically add the host key to the list of known hosts
